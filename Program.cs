0c3111a5809fad4ff6d99a7aa506a1b351cf209f
Danyelle Santana <dany.san120404@gmail.com>
2022-05-30 11:15:31 -0300

    enhancement: add delete method

--- Program.cs
+++ Program.cs
@@ -34,46 +34,11 @@ if(modelAction == "New")
     computerRepository.Save(computer);
 }
 
-if(modelName == "Lab")
+if(modelAction == "Delete")
 {
-    if(modelAction == "List")
-    {
-        var connection =  new SqliteConnection("Data Source=database_test.db");
-        connection.Open();
-        var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Labs";
-
-        var reader = command.ExecuteReader();
-
-        Console.WriteLine("Lab List");
-        while(reader.Read())
-        {
-            Console.WriteLine(
-                "{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3));
-        }
-
-        reader.Close();
-        connection.Close();
-    }
-}
-
-if(modelAction == "New")
-{
-    var connection =  new SqliteConnection("Data Source=database_test.db");
-    connection.Open();
-
     int id = Convert.ToInt32(args[2]);
-    int numero = Convert.ToInt32(args[3]);
-    string nome = args[4];
-    string bloco = args[5];
 
-    var command = connection.CreateCommand();
-    command.CommandText = "INSERT INTO Computers VALUES($id, $number, $name, $block)";
-    command.Parameters.AddWithValue("$id", id);
-    command.Parameters.AddWithValue("$number", numero);
-    command.Parameters.AddWithValue("$name", nome);
-    command.Parameters.AddWithValue("$block", bloco);
-
-    command.ExecuteNonQuery();
-    connection.Close();
+    computerRepository.Delete(id);
+    
 }
+
